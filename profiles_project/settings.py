7449c56640743976d805daf1087d1769ee6577d4
Tanthunder <tanmaysshinde@gmail.com>
2022-11-17 14:18:46 +0530

    Added custom user profile model, manager,migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#setting custom user model
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
